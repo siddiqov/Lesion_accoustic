c81b28fc6a5bbde3e9b3db28bc1cb014f540c14e
Hafeez <siddiqov@gmail.com>
2024-06-05 05:49:01 +0100

    Model developed and evaluted in notebook

--- src/exception.py
+++ src/exception.py
@@ -17,9 +17,4 @@ class CustomerException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomerException(e, sys)
+

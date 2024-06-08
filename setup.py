125ef0f0b550bc3e8c0be7e8a3cea754a94dbc2c
Hafeez <siddiqov@gmail.com>
2024-06-08 11:07:58 +0100

    spell mistake in setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT = '-e .'
+HYPHEN_E_DOT = '-e .'
 
 def get_requirements(file_path: str) -> List[str]:
     '''
-    This function will return the list of requirements
+    This function returns a list of requirements.
     '''
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
+        requirements = [req.strip() for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
 
     return requirements
 
